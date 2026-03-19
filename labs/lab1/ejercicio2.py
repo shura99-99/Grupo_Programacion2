87d1163db5eaf9065ce132e55c71a42df1030b23
shura99-99 <76755377+shura99-99@users.noreply.github.com>
2026-03-18 22:49:12 -0300

    Add battery discharge calculation script
    Implement battery discharge calculation based on initial and minimum voltage.

--- labs/lab1/ejercicio2.py
+++ labs/lab1/ejercicio2.py
@@ -1,1 +1,8 @@
-
+#Ejercicio 2 – Descarga de un Banco de Baterías
+Volatje_inicial = float(input("Ingrese el valor de voltaje "))
+Voltaje_minimio = float(input("Ingrese el valor de voltaje minimo de operacion "))
+Horas = 0
+while Volatje_inicial > Voltaje_minimio:
+    Volatje_inicial *= 0.97
+    Horas += 1
+print("El banco de baterias logro entregar energia durante ", int(Horas), "horas")
